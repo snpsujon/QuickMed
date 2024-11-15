f38479d80d467151cffb6a3563f2560c5ed9041d
Munshi Faysal Ahmed <fmunshi752@gmail.com>
2024-11-16 05:48:18 +0600

    ghdfhdh

--- QuickMed/Interface/IDurationTemp.cs
+++ QuickMed/Interface/IDurationTemp.cs
@@ -1,7 +1,12 @@
-﻿namespace QuickMed.Interface
+﻿using QuickMed.DB;
+
+namespace QuickMed.Interface
 {
     public interface IDurationTemp : IBase
     {
         Task<dynamic> GetAsync();
+        Task<dynamic> SaveAsync(TblDuration data);
+        Task<dynamic> UpdateAsync(TblDuration data);
+        Task<dynamic> DeleteAsync(Guid id);
     }
 }
--- QuickMed/Services/DurationTempService.cs
+++ QuickMed/Services/DurationTempService.cs
@@ -21,5 +21,49 @@ namespace QuickMed.Services
                 throw;
             }
         }
+
+        public async Task<dynamic> SaveAsync(TblDuration data)
+        {
+            try
+            {
+                var saveTemplate = await _context.CreateAsync<TblDuration>(data);
+                return true;
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+        }
+
+        public async Task<dynamic> UpdateAsync(TblDuration data)
+        {
+            try
+            {
+                int rowsAffected = await _context.UpdateAsync(data);
+                return rowsAffected > 0;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("An error occurred while updating the template.", ex);
+            }
+        }
+        public async Task<dynamic> DeleteAsync(Guid id)
+        {
+            try
+            {
+                var q = $"DELETE FROM TblDuration WHERE Id = '{id}'";
+
+                var deleteResult = await _context.ExecuteSqlQueryFirstorDefultAsync<TblDuration>(q);
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+
+        }
     }
 }
