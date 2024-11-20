1923611086d91395810f648430b7538dd07465c8
MD Mehedi Hasan <128966181+sheikh-mehedi@users.noreply.github.com>
2024-11-20 15:11:09 +0600

    dddd

--- QuickMed/BaseComponent/BaseDoseTemp.cs
+++ QuickMed/BaseComponent/BaseDoseTemp.cs
@@ -13,54 +13,71 @@ namespace QuickMed.BaseComponent
         [Inject]
         public IJSRuntime JS { get; set; }
 
-        public TblDose model = new();
-        public IEnumerable<TblDose>? models { get; set; }
+        public TblDose dose = new();
+        public IEnumerable<TblDose>? doses { get; set; }
 
         protected override async Task OnInitializedAsync()
         {
-            models = await _dose.GetAsync(); // Load the initial data
+            doses = await App.Database.GetTableRowsAsync<TblDose>("TblDose");
+            doses = await _dose.GetAsync(); // Load the initial data
         }
 
         protected override async Task OnAfterRenderAsync(bool firstRender)
         {
             if (firstRender)
             {
-                await InitializeDataTable(); // Initialize JavaScript-based DataTable once the component has rendered
+                await RefreshDataTable(); // Initialize JavaScript-based DataTable once the component has rendered
             }
         }
 
-        protected async Task InitializeDataTable()
+        //protected async Task InitializeDataTable()
+        //{
+        //    await JS.InvokeVoidAsync("makeDataTable", "datatable-patientList");
+        //}
+
+        protected async Task RefreshDataTable()
         {
-            await JS.InvokeVoidAsync("makeDataTable", "datatable-patientList");
+
+            var tableData = doses?.Select((ds, index) => new[]
+                {
+                    (index + 1).ToString(), // Serial number starts from 1
+                    ds.Name?.ToString() ?? string.Empty, // Note name
+                    $@"
+                    <div style='display: flex; justify-content: flex-end;'>
+                        <i class='dripicons-pencil btn btn-soft-primary' onclick='editRow({ds.Id})'></i>
+                        <i class='dripicons-trash btn btn-soft-danger' onclick='deleteRow({ds.Id})'></i>
+                    </div>
+                    " // Action buttons
+                }).ToArray();
+
+            await JS.InvokeVoidAsync("makeDataTableQ", "datatable-doseTemp", tableData);
+
         }
 
         protected async Task OnSaveBtnClick()
         {
-            if (model.Id == Guid.Empty) // Check if the GUID is uninitialized
+            if (dose.Id == Guid.Empty) // Check if the GUID is uninitialized
             {
-                model.Id = Guid.NewGuid(); // This line will be redundant as the default is already set
-                await _dose.SaveAsync(model); // Create the new template
+                dose.Id = Guid.NewGuid(); // This line will be redundant as the default is already set
+                await _dose.SaveAsync(dose); // Create the new template
                 await JS.InvokeVoidAsync("showAlert", "Save Successful", "Record has been successfully Saved.", "success", "swal-success");
             }
             else
             {
-                await _dose.UpdateAsync(model); // Update the existing template
+                await _dose.UpdateAsync(dose); // Update the existing template
                 await JS.InvokeVoidAsync("showAlert", "Update Successful", "Record has been successfully Updated.", "success", "swal-info");
             }
 
             // Reset the model for future input
-            model = new TblDose(); // Creates a new instance with a new GUID
-
-            // Fetch updated data
-            //models = await _dXTemp.GetCCTempData();
+            dose = new TblDose(); // Creates a new instance with a new GUID
             await OnInitializedAsync();
-
+            await RefreshDataTable();
             StateHasChanged();  // Update the UI
         }
 
         protected async Task OnEditClick(TblDose data)
         {
-            model = data;
+            dose = data;
             StateHasChanged(); // Re-render the component with the updated model
         }
         protected async Task OnDeleteClick(Guid id)
