18333db13eb6fd0285afa2f26fb6d5c6c943bd02
Munshi Faysal Ahmed <fmunshi752@gmail.com>
2024-11-21 02:21:01 +0600

    mmmmm

--- QuickMed/Services/FavouriteDrugService.cs
+++ QuickMed/Services/FavouriteDrugService.cs
@@ -1,5 +1,6 @@
 ﻿using QuickMed.DB;
 using QuickMed.Interface;
+using QuickMed.ViewModels;
 
 namespace QuickMed.Services
 {
@@ -12,7 +13,30 @@ namespace QuickMed.Services
         {
             try
             {
-                return await _context.GetTableRowsAsync<TblFavouriteDrugTemplate>("TblFavouriteDrugTemplate");
+                var query = @"
+                        select fd.Id,fd.Name,dm.Id as BrandId,dm.Name as BrandName,dd.Id as DoseId,dd.Name as DoseName,
+                        d.Id as DurationId,d.Name as DurationName,i.Id as InstructionId,i.Name as InstructionName FROM TblFavouriteDrugTemplate fd
+                        LEFT JOIN DrugMedicine dm on fd.BrandId = dm.Id
+                        LEFT JOIN TblDose dd ON fd.DoseId = dd.Id
+                        LEFT JOIN TblDuration d ON fd.DurationId = d.Id
+                        LEFT JOIN TblInstruction i ON fd.InstructionId = i.Id";
+
+                var result = await _context.ExecuteSqlQueryAsync<FavouriteDrugTempVM>(query);
+                return result;
+                //return await _context.GetTableRowsAsync<TblFavouriteDrugTemplate>("TblFavouriteDrugTemplate");
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+        }
+        public async Task<dynamic> SaveFavouriteDrugTemp(TblFavouriteDrugTemplate TblFavouriteDrugTemplate)
+        {
+            try
+            {
+                var saveTemplate = await _context.CreateAsync<TblFavouriteDrugTemplate>(TblFavouriteDrugTemplate);
+                return true;
             }
             catch (Exception ex)
             {
---
+++ QuickMed/wwwroot/js/favDrug.js
@@ -0,0 +1,18 @@
+
+function GetDrugTempData() {
+
+    const tempname = $("#TempName").val();
+    const brandSelect = $("#brandSelect").val();
+    const doseSelect = $("#doseSelect").val();
+    const instructionSelect = $("#instructionSelect").val();
+    const durationSelectfav = $("#durationSelectfav").val();
+
+    var data = {
+        templateName: tempname == '' ? 'MixTemp_' + getRandomInteger(1, 9999) : tempname,
+        brandSelect: brandSelect,
+        doseSelect: doseSelect,
+        instructionSelect: instructionSelect,
+        durationSelectfav: durationSelectfav
+    };
+    return data;
+}
