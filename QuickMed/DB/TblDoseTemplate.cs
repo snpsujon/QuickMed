b365b1354f1f0383fc0c8c39d9054872b74f66ce
Shoaib Ullah Rasel <56650099+shoaibrasell@users.noreply.github.com>
2024-11-20 12:04:23 +0600

    sssss

---
+++ QuickMed/DB/TblDoseTemplate.cs
@@ -0,0 +1,17 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickMed.DB
+{
+    
+    public class TblDoseTemplate
+    {
+        [PrimaryKey]
+        public Guid Id { get; set; }
+        public string? Name { get; set; }
+    }
+}
