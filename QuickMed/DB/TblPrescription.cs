fe28e5f020e0a136937c560b0e501f2af90a9ccd
Shoaib Ullah Rasel <56650099+shoaibrasell@users.noreply.github.com>
2024-11-21 21:01:23 +0600

    Added Table

--- QuickMed/DB/TblPrescription.cs
+++ QuickMed/DB/TblPrescription.cs
@@ -8,27 +8,21 @@ namespace QuickMed.DB
         public Guid Id { get; set; }
         public string? LicenseKey { get; set; }  // Foreign key reference to TblDoctor
         public Guid? PatientId { get; set; }  // Foreign key reference if needed
-        public string? Dx { get; set; }
         public DateTime? PrescriptionDate { get; set; }
-        public string? MobileNumber { get; set; }
-        public string? Address { get; set; }
-        public string? Plan { get; set; }
-        public string? CC { get; set; }  // Chief Complaint
-        public string? OE { get; set; }  // Other Examination findings
-        public string? IX { get; set; }  // Investigations
-        public string? Advice { get; set; }  // Advice for the patient
-        public string? PrescriptionDetails { get; set; }  // Advice for the patient
-        public decimal? NumberAfter { get; set; }  // Numeric value, perhaps dosage or quantity
-        public bool? ISDayAfter { get; set; }  // Indicates if the prescription is for the day after
+        public string? PrescriptionCode { get; set; }
         public bool? ISMonthAfter { get; set; }  // Indicates if the prescription is for the month after
-
-
+        public DateTime? NextMeetingDate { get; set; }
+        public int? NextMeetingValue { get; set; }
+        public int? Payment { get; set; }
+        public string? RefferedBy { get; set; }
+        public int? AdviceTempId { get; set; }
+        public int? TreatmentTempId { get; set; }
+        public int? Height { get; set; }
+        public int? weight { get; set; }
         public string? CreatedBy { get; set; } = string.Empty;
         public DateTime? CreatedAt { get; set; } = DateTime.Now; // Use UTC time for consistency
         public string? UpdatedBy { get; set; } = string.Empty;
         public DateTime? UpdatedAt { get; set; } = DateTime.Now;
-        public string? PrescriptionCode { get; set; }
-
         public bool IsSynced { get; set; } = false; // Default to false for new records
     }
 
