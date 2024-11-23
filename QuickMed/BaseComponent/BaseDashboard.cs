60d805f800843428bb299515d0e7ab89cfdd29f9
MD Mehedi Hasan <128966181+sheikh-mehedi@users.noreply.github.com>
2024-11-23 14:59:42 +0600

    cccc

---
+++ QuickMed/BaseComponent/BaseDashboard.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using QuickMed.DB;
+using QuickMed.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickMed.BaseComponent
+{
+    public class BaseDashboard : ComponentBase
+    {
+        [Inject]
+        public IDashboard _dashboard { get; set; }
+
+        [Inject]
+        public IJSRuntime JS { get; set; }
+
+        public TblPrescription dose = new();
+        public IEnumerable<TblDose>? doses { get; set; }
+        public DotNetObjectReference<BaseDashboard> ObjectReference { get; private set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            ObjectReference = DotNetObjectReference.Create(this);
+            await JS.InvokeVoidAsync("setInstanceReferenceForAll", ObjectReference);
+            doses = await App.Database.GetTableRowsAsync<TblDose>("TblDose");
+            doses = await _dashboard.GetMostUsedDXAsync(); // Load the initial data
+            await RefreshDataTable();
+        }
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+            {
+                await RefreshDataTable(); // Initialize JavaScript-based DataTable once the component has rendered
+            }
+        }
+        protected async Task RefreshDataTable()
+        {
+
+            var tableData = doses?.Select((ds, index) => new[]
+                {
+                    (index + 1).ToString(), // Serial number starts from 1
+                    ds.Name?.ToString() ?? string.Empty, // Note name
+                    $@"
+                    <div style='display: flex; justify-content: flex-end;'>
+                        <i class='dripicons-pencil btn btn-soft-primary dTRowActionBtn' data-id='{ds.Id}' data-method='OnEditClick'></i>
+                        <i class='dripicons-trash btn btn-soft-danger dTRowActionBtn' data-id='{ds.Id}' data-method='OnDeleteClick'></i>
+                    </div>
+                    " // Action buttons
+                }).ToArray();
+
+            await JS.InvokeVoidAsync("makeDataTableQ", "datatable-doseTemp", tableData);
+
+        }
+    }
+}
---
+++ QuickMed/Services/DashboardService.cs
@@ -0,0 +1,27 @@
+﻿using QuickMed.DB;
+using QuickMed.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickMed.Services
+{
+    public class DashboardService : BaseServices, IDashboard
+    {
+        public DashboardService(ApplicationDbContext context) : base(context)
+        {
+        }
+
+        public async Task<dynamic> GetMostUsedDXAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<dynamic> GetMostUsedMedicineAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
