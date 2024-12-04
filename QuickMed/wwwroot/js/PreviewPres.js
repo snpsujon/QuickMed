45ecf51b5e36abf1441bc5f9df8245e962cc300c
Munshi Faysal Ahmed <fmunshi752@gmail.com>
2024-12-05 00:25:47 +0600

    kkk

--- QuickMed/wwwroot/js/PreviewPres.js
+++ QuickMed/wwwroot/js/PreviewPres.js
@@ -16,3 +16,16 @@
 
 }
 
+function getPrintContent() {
+    var printContents = document.getElementById('printDiv').innerHTML;
+    originalContents = document.body.innerHTML;
+    document.body.innerHTML = printContents;
+    var nowContent = document.body.innerHTML;
+
+    if (!printContents) {
+        alert("Element with id 'printDiv' not found");
+        return null;
+    }
+    return nowContent;
+
+}
