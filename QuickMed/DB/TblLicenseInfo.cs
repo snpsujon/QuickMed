e41e12e3847d8ea6de5bdd8f63a2a455c3e54a66
snpsujon <snpsujon93@gmail.com>
2024-12-21 03:44:34 +0600

    huge change

--- QuickMed/DB/TblLicenseInfo.cs
+++ QuickMed/DB/TblLicenseInfo.cs
@@ -8,6 +8,7 @@ namespace QuickMed.DB
         public Guid Id { get; set; }
         public string LicenseKey { get; set; }
         public DateTime ExpirationDate { get; set; }
+        public string? SoftVersion {get;set;}
 
     }
     public class LicenseApiResponse
@@ -20,5 +21,6 @@ namespace QuickMed.DB
         public int IsValid { get; set; }
         public DateTime ExpirationDate { get; set; }
         public TblDoctor Doctor { get; set; }
+        public string? SoftVersion {get;set;}
     }
 }
